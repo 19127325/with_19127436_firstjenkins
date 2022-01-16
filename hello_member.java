185a21ed4e970a9f86280be59cc44b6a81111ce8
An <97687144+19127325@users.noreply.github.com>
2022-01-16 22:39:15 +0700

    Update hello_member.java

--- hello_member.java
+++ hello_member.java
@@ -3,6 +3,6 @@ public class hello_member {
         System.out.println("Assignment 3");
         System.out.println("19127325 - Nguyen Huu Hoang An");
         System.out.println("19127436 - Tang Tuong Khang");
-        System.out.println("New member: Chuc thay co nam moi vui ve - An Khang Thinh Vuong - Van Su Nhu Y....");
+        System.out.println("New member: Chuc thay co nam moi vui ve - An Khang Thinh Vuong - Van Su Nhu Y..");
     }
 }
