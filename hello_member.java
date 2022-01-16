331fea5e70a1ab021c9fca9a88c5a8b04deec628
An <hoangan2626@gmail.com>
2022-01-16 14:53:44 +0700

    Create a simple java file

---
+++ hello_member.java
@@ -0,0 +1,8 @@
+public class hello_member {
+    public static void main(String[] args) {
+        System.out.println("Assignment 3");
+        System.out.println("19127325 - Nguyen Huu Hoang An");
+        System.out.println("19127436 - Tang Tuong Khang");
+        System.out.println("New member: An Khang Thinh Vuong - Van Su Nhu Y");
+    }
+}
