2cb1ba152cb200bcf28f75463a96a28a5184dfe6
An <hoangan2626@gmail.com>
2022-01-16 15:08:10 +0700

    Delete hello_member.java

--- hello_member.java
+++
@@ -1,8 +0,0 @@
-public class hello_member {
-    public static void main(String[] args) {
-        System.out.println("Assignment 3");
-        System.out.println("19127325 - Nguyen Huu Hoang An");
-        System.out.println("19127436 - Tang Tuong Khang");
-        System.out.println("New member: Chuc thay co nam moi vui ve - An Khang Thinh Vuong - Van Su Nhu Y");
-    }
-}
